ee9a3ff11fd2bb3b4177f758cba5bf7d826cc560
raul <raul@raul-VirtualBox>
2014-10-09 18:49:17 +0200

    First commit

---
+++ src/main/java/edu/upc/eetac/dsa/rgonzalez/Ejercicio8/Main.java
@@ -0,0 +1,16 @@
+package edu.upc.eetac.dsa.rgonzalez.Ejercicio8;
+
+
+
+public class Main {
+	public static void main(String[] args){
+		
+		//Instanciamos o creamos los objetos
+		CuentaAtras hilo1 = new CuentaAtras("Hilo 1");
+	
+		
+		hilo1.start();
+		
+		
+	}
+}
